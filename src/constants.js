a3fd983c8a51347a034f4123dcb5a997efaf236b
ovidiumilitaru <militaru.ovidiu@gmail.com>
2023-11-05 21:33:32 +0200

    added React and constants files

---
+++ src/constants.js
@@ -0,0 +1,27 @@
+export const songsList = [
+  {
+    authod: 'Metallica',
+    title: 'Whiskey in the Jar',
+    id: 'Metallica01SONGWhiskeyintheJar'
+  }, 
+  {
+    author: 'Beatles',
+    title: 'Let It Be',
+    id: 'Beatles01SONGLet It Be'
+  },
+  {
+    author: 'Queen',
+    title: 'We Are the Champions',
+    id: 'Queen01SONG'
+  },
+  {
+    author: 'Sia',
+    title: 'Gimme Love',
+    id: 'Sia01SONGGimme Love'
+  },
+  {
+    author: 'Dua Lipa',
+    title: 'Dance the Night',
+    id: 'DuaLipa01SONGDancetheNight'
+  }
+]
